b01f6adcf764354bfa3af397d01f5d9cdb1122c2
Samuel Bentil <ebobentil@yahoo.com>
2016-10-27 12:57:12 +0000

    ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).on("pagecreate", function() {
 	$("#splahLabel1").slideDown(3000, function() {
 		// window.location = 'login.php';
-		window.location = 'http://216.224.161.207/wisdom/login.php'; 
+		window.location = 'http://api.nalosolutions.com/wisdom/login.php';
 	});
 });
  
